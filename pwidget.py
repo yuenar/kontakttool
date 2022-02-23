563f3e98046b1d13a7ca828a9100efb34046fd85
yuenar <yuenar2@gmail.com>
2022-02-23 15:03:02 +0800

    First commit

---
+++ pwidget.py
@@ -0,0 +1,145 @@
+# This Python file uses the following encoding: utf-8
+import os
+from pathlib import Path
+import sys
+
+from PySide6.QtWidgets import QPushButton, QWidget,QFileDialog,QLineEdit, QTextBrowser,QMessageBox
+from PySide6.QtGui import QRegularExpressionValidator,QImage
+from PySide6.QtCore import QFile,QRegularExpression, Signal
+from PySide6.QtUiTools import QUiLoader
+
+def create_nicnt(compyName,libName,snpid,path):
+    if os.path.isdir(path):
+        src = os.fspath(Path(__file__).resolve().parent /"src/Source.nicnt")
+        fullname=libName+".nicnt"
+        fullpath=os.path.join(path,fullname)
+
+        fp3 = open(src, "r", encoding="ISO-8859-1")
+        fp4 = open(fullpath, "w")
+
+        for s in fp3.readlines():  # 先读出来
+            fp4.write(s.replace("libName", libName).replace("sid",snpid).replace("compName",compyName))  # 替换 并写入
+
+        fp3.close()
+        fp4.close()
+        # print("done")
+        return fullpath
+
+class PWidget(QWidget):
+    importPath = Signal(str)
+    def __init__(self):
+        super(PWidget, self).__init__()
+        self.load_ui()
+        self.dialog = QFileDialog()
+        wpBtn=QPushButton("打开Broswer", self,
+                                     objectName="GreenButton", minimumHeight=32 ,clicked=self.openFolder)
+        wpBtn.move(250,280)
+        wpBtn.resize(100,36)
+
+
+        opBtn=QPushButton("选图Take Image", self,
+                                     objectName="GreenButton", minimumHeight=32 ,clicked=self.openPic)
+        opBtn.move(250,360)
+        opBtn.resize(100, 36)
+
+        cBtn=QPushButton("生成并导入库Create .nicnt && Export to Library ", self,
+                                     objectName="GreenButton", minimumHeight=32 ,clicked=self.doCreateNcint)
+        cBtn.move(20,460)
+        cBtn.resize(330, 32)
+
+        vtor=QRegularExpressionValidator()
+        vtor.setRegularExpression(QRegularExpression("[^%&',;=?$\x22]+[a-zA-Z0-9]+$"))
+
+        self.cle=QLineEdit(self)
+        self.cle.setPlaceholderText("不支持输入中文！Cannot input Chinese！")
+        self.cle.setMaxLength(128)
+        self.cle.move(20,60)
+        self.cle.resize(330,36)
+        self.cle.setValidator(vtor)
+
+        self.ble=QLineEdit(self)
+        self.ble.setPlaceholderText("不支持输入中文！Cannot input Chinese！")
+        self.ble.move(20, 150)
+        self.ble.setMaxLength(128)
+        self.ble.resize(330,36)
+        self.ble.setValidator(vtor)
+
+        self.sle=QLineEdit(self)
+        self.sle.setPlaceholderText("最多输入4位字符！Enter up to 4 characters！")
+        self.sle.setText("a123")
+
+        vtor1=QRegularExpressionValidator()
+        vtor1.setRegularExpression(QRegularExpression("[a-zA-Z0-9]+$"))
+        self.sle.setValidator(vtor1)
+        self.sle.setMaxLength(4)
+        self.sle.move(20, 240)
+        self.sle.resize(330, 36)
+
+        self.ole=QLineEdit(self)
+        self.ole.setPlaceholderText("选择输出路径，choose a floder.")
+        self.ole.move(20, 320)
+        self.ole.resize(330, 36)
+
+        self.wle = QLineEdit(self)
+        self.wle.setPlaceholderText("选择一张图片做背景，choose a wallpaper.")
+        self.wle.move(20, 410)
+        self.wle.resize(330, 36)
+
+        self.msgBox = QMessageBox()
+        self.msgBox.setWindowTitle('警告Warning')
+        self.msgBox.setIcon(QMessageBox.Warning)
+        self.msgBox.setInformativeText("请检查输入Please check your input！")
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.fspath(Path(__file__).resolve().parent /"src/form.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+    def openFolder(self):
+        lib_dir = self.dialog.getExistingDirectory(self, "选取文件夹", os.getcwd())
+        if len(lib_dir) < 1:
+            return
+
+        # print(lib_dir)
+
+        self.ole.setText(lib_dir)
+
+    def openPic(self):
+        lib_dir = self.dialog.getOpenFileName(self,"please choose an image file",os.getcwd(),"Image Files(*.jpg *.png )")
+        if len(lib_dir) < 1:
+            return
+
+        self.wle.setText(lib_dir[0])
+
+    def doCreateNcint(self):
+        if(len(self.cle.text())<1):
+            self.msgBox.setText("公司名不得为空Cannot input null！")
+            self.msgBox.show()
+        elif (len(self.ble.text())<1):
+            self.msgBox.setText("音色名不得为空Cannot input null！")
+            self.msgBox.show()
+        elif (len(self.sle.text())<1):
+            self.msgBox.setText("音色id不得为空Cannot input null！")
+            self.msgBox.show()
+        elif (len(self.ole.text())<1):
+            self.msgBox.setText("路径不得为空Cannot input null！")
+            self.msgBox.show()
+        elif not os.path.exists(self.ole.text()):
+            self.msgBox.setText("路径不存在Is not exist！")
+            self.msgBox.show()
+        else:
+            # print("creating...")
+            path= create_nicnt(self.cle.text(),self.ble.text(),self.sle.text(),self.ole.text())
+            self.importPath.emit(path)
+            outPic = self.ole.text() + "/wallpaper.png"
+
+            if not os.path.exists(self.wle.text()):
+                src=os.fspath(Path(__file__).resolve().parent /"src/wallpaper.png")
+                img=QImage(src)
+                img.scaled(450, 100).save(outPic)
+            else:
+                img=QImage(self.wle.text())
+                img.scaled(450,100).save(outPic)
---
+++ widget.py
@@ -0,0 +1,401 @@
+# This Python file uses the following encoding: utf-8
+# -*- coding: utf-8 -*-
+
+import sys , os
+
+import re
+import stat
+from PySide6.QtGui import QDesktopServices,QPixmap
+from PySide6.QtCore import QUrl
+from PySide6.QtWidgets import QMessageBox,QFileDialog
+from biplist import *
+import xmltodict
+from pwidget import PWidget
+from pathlib import Path
+
+from PySide6.QtCore import QSize, Signal as pyqtSignal
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLineEdit, QPushButton, \
+        QListWidgetItem, QVBoxLayout, QListWidget, QApplication
+
+
+class ItemWidget(QWidget):
+    itemDeleted = pyqtSignal(QListWidgetItem)
+
+    def __init__(self, text, item, *args, **kwargs):
+        super(ItemWidget, self).__init__(*args, **kwargs)
+        self._item = item  # 保留list item的对象引用
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(QLineEdit(text, self))
+        layout.addWidget(QPushButton('x', self, clicked=self.doDeleteItem))
+
+    def doDeleteItem(self):
+        self.itemDeleted.emit(self._item)
+
+    def sizeHint(self):
+        # 决定item的高度
+        return QSize(200, 40)
+
+StyleSheet = """
+/*这里是通用设置，所有按钮都有效，不过后面的可以覆盖这个*/
+QPushButton {
+    border: none; /*去掉边框*/
+}
+/*
+QPushButton#xxx
+或者
+#xx
+都表示通过设置的objectName来指定
+*/
+QPushButton#RedButton {
+    background-color: #f44336; /*背景颜色*/
+}
+#RedButton:hover {
+    background-color: #e57373; /*鼠标悬停时背景颜色*/
+}
+/*注意pressed一定要放在hover的后面，否则没有效果*/
+#RedButton:pressed {
+    background-color: #ffcdd2; /*鼠标按下不放时背景颜色*/
+}
+#GreenButton {
+    background-color: #4caf50;
+    border-radius: 5px; /*圆角*/
+}
+#GreenButton:hover {
+    background-color: #81c784;
+}
+#GreenButton:pressed {
+    background-color: #c8e6c9;
+}
+#BlueButton {
+    background-color: #2196f3;
+    /*限制最小最大尺寸*/
+    min-width: 96px;
+    max-width: 96px;
+    min-height: 96px;
+    max-height: 96px;
+    border-radius: 48px; /*圆形*/
+}
+#BlueButton:hover {
+    background-color: #64b5f6;
+}
+#BlueButton:pressed {
+    background-color: #bbdefb;
+}
+#OrangeButton {
+    max-height: 48px;
+    border-top-right-radius: 20px; /*右上角圆角*/
+    border-bottom-left-radius: 20px; /*左下角圆角*/
+    background-color: #ff9800;
+}
+#OrangeButton:hover {
+    background-color: #ffb74d;
+}
+#OrangeButton:pressed {
+    background-color: #ffe0b2;
+}
+/*根据文字内容来区分按钮,同理还可以根据其它属性来区分*/
+QPushButton[text="purple button"] {
+    color: white; /*文字颜色*/
+    background-color: #9c27b0;
+}
+"""
+
+TARGET_XML_DIR = "/Library/Application Support/Native Instruments/Service Center"
+TARGET_PLIST_DIR = "/Library/Preferences"
+
+# TARGET_PLIST_DIR = "/Volumes/misc/test/Preferences"
+# TARGET_XML_DIR = "/Volumes/misc/test/Service Center"
+
+RD, WD, XD = 4, 2, 1
+BNS = [RD, WD, XD]
+MDS = [
+    [stat.S_IRUSR, stat.S_IRGRP, stat.S_IROTH],
+    [stat.S_IWUSR, stat.S_IWGRP, stat.S_IWOTH],
+    [stat.S_IXUSR, stat.S_IXGRP, stat.S_IXOTH]
+]
+
+def chmod(path, mode):
+    if isinstance(mode, int):
+        mode = str(mode)
+    if not re.match("^[0-7]{1,3}$", mode):
+        raise Exception("mode does not conform to ^[0-7]{1,3}$ pattern")
+    mode = "{0:0>3}".format(mode)
+    mode_num = 0
+    for midx, m in enumerate(mode):
+        for bnidx, bn in enumerate(BNS):
+            if (int(m) & bn) > 0:
+                mode_num += MDS[bnidx][midx]
+    os.chmod(path, mode_num)
+
+def list_all_files(rootdir):
+    import os
+    _files = []
+    list = os.listdir(rootdir) #列出文件夹下所有的目录与文件
+    for i in range(0,len(list)):
+           path = os.path.join(rootdir,list[i])
+           if os.path.isdir(path):
+              _files.extend(list_all_files(path))
+           if os.path.isfile(path):
+              _files.append(path)
+    return _files
+
+
+def parse_ncint(path):
+    # f = open(path, "r", encoding='unicode_escape')
+    f = open(path, "r", encoding="ISO-8859-1")
+    baseName=os.path.basename(path)
+    libName=baseName.replace(".nicnt", ".")
+
+    lines = f.readlines()  # 读取全部内容 ，并以列表方式返回
+    newXML='<?xml version="1.0" encoding="UTF-8" standalone="no" ?>\n'
+
+    for line in lines[1:] : #循环读取每一行，1：是从第二行开始
+        if "</ProductHints>" in line:
+            newXML+=line
+            # print("find:==============="+line)
+            f.close()
+            break
+        else:
+            newXML += line
+
+    f = libName + "xml"
+    full_new_xml = os.path.join(TARGET_XML_DIR, f)
+    # 以只写模式打开我们的文本文件以写入替换的内容
+    with open(full_new_xml, 'w', encoding='UTF-8') as file:
+        # 在我们的文本文件中写入替换的数据
+        file.write(newXML)
+    chmod(full_new_xml, "755")
+    # print("xml:"+newXML)
+    d = xmltodict.parse(newXML)
+    spath = path.replace(baseName,"").replace("/Volumes/","").replace("/",":")
+    # # spath=
+    if('HU'in d["ProductHints"]["Product"]["ProductSpecific"]):
+        hu=d["ProductHints"]["Product"]["ProductSpecific"]['HU']
+    else:
+        hu=''
+    if('JDX'in d["ProductHints"]["Product"]["ProductSpecific"]):
+        jdx=d["ProductHints"]["Product"]["ProductSpecific"]['JDX']
+    else:
+        jdx=''
+
+    if('UPID'in d["ProductHints"]["Product"]):
+        upid=d["ProductHints"]["Product"]['UPID']
+    else:
+        upid=''
+
+    create_plist(d["ProductHints"]["Product"]['Name'], d["ProductHints"]["Product"]['RegKey'], hu, upid, spath,jdx, d["ProductHints"]["Product"]['SNPID'])
+    return full_new_xml
+
+def create_plist(libName, RegKey, HU,UPID,path,JDX, snpid):
+    f = "com.native-instruments."+libName+".plist"
+    full_new_plist = os.path.join(TARGET_PLIST_DIR, f)
+    src = os.fspath(Path(__file__).resolve().parent /"src/Source.plist")
+    plist = readPlist(src)
+    plist['Name'] = libName
+    plist['RegKey'] = RegKey
+    plist['SNPID'] = snpid
+    plist['UPID'] = UPID
+    plist['HU'] = HU
+    plist['JDX'] = JDX
+    plist['ContentDir'] = path
+
+    writePlist(plist, full_new_plist)
+    chmod(full_new_plist, "755")
+    # print("===dst plist:" + full_new_plist)
+
+def judge_ktxml(apath):
+    if not os.path.isfile(apath):
+        return
+
+    # print(apath)
+    baseName = os.path.basename(apath)
+    if(len(baseName)<5):
+        return
+
+    f = open(apath, "r", encoding="UTF-8")
+
+
+    adata = f.read()
+
+    d = xmltodict.parse(adata)
+    if "Plugin" in d["ProductHints"]["Product"]["Type"]:
+        return ""
+    else:
+        return baseName.replace(".xml" , "")
+
+class Widget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setWindowTitle("康泰克助手Kontakt Tool by OwenZhang张礼乐")
+        self.dialog = QFileDialog()
+        self.list=[]
+        self.setupUi()
+
+        self.resize(800,600)
+        self.setMinimumSize(800,600)
+        self.setMaximumSize(800,600)
+        self.getFullLibs()
+
+    def doDeleteItem(self, item):
+        # 根据item得到它对应的行数
+        row = self.listWidget.indexFromItem(item).row()
+        # 删除item
+
+        name=self.list[row]
+        os.remove(name)
+        # print("del:"+name)
+        del self.list[row]
+        item = self.listWidget.takeItem(row)
+        # 删除widget
+        self.listWidget.removeItemWidget(item)
+        del item
+
+    def doClearItem(self):
+        if(self.listWidget.count()<1):
+            return
+
+        msgBox = QMessageBox(self)
+
+        msgBox.resize(360,240)
+        msgBox.setWindowTitle('警告Warning')
+        msgBox.setIcon(QMessageBox.Warning)
+        msgBox.setText("是否清空所有入库音色？操作不可还原！Clear all banked tones? The operation is irreversible!")
+        okButton = msgBox.addButton(self.tr("确定Go on"), QMessageBox.ActionRole)
+        okButton.clicked.connect(self.realClear)
+        msgBox.exec_()
+
+    def realClear(self):
+        # 清空所有Item
+        for _ in range(self.listWidget.count()):
+            # 删除item
+            # 一直是0的原因是一直从第一行删,删掉第一行后第二行变成了第一行
+            # 这个和删除list [] 里的数据是一个道理
+            item = self.listWidget.takeItem(0)
+            name = self.list[0]
+            del self.list[0]
+            os.remove(name)
+            # 删除widget
+            self.listWidget.removeItemWidget(item)
+            del item
+
+        self.list.clear()
+
+    def doEmail(self):
+        QDesktopServices.openUrl(QUrl("mailto:yuenar2@gmail.com"))
+
+    # def doHelper(self):
+    #     self.sw.setCurrentIndex(2)
+
+    def doDonate(self):
+        msgBox = QMessageBox(self)
+        msgBox.resize(360,240)
+        # msgBox.setText("Alipay支付宝")
+        msgBox.setInformativeText("支付宝向我捐赠？Donate by alipay?")
+        src = os.fspath(Path(__file__).resolve().parent /"src/alipay.png")
+        p = QPixmap(src)
+        msgBox.setIconPixmap(p.scaled(256, 256))
+        palButton = msgBox.addButton(self.tr("Or Paypal或贝宝？"), QMessageBox.ActionRole)
+        palButton.clicked.connect(self.paypal)
+        msgBox.exec()
+
+    def paypal(self):
+        QDesktopServices.openUrl(QUrl("https://www.paypal.com/paypalme/yuenar"))
+
+    def doFollow(self):
+        QDesktopServices.openUrl(QUrl("https://y.qq.com/n/ryqq/singer/0044yxPF1Zultc"))
+
+    def setupUi(self):
+        mainLay= QVBoxLayout(self)
+        layout = QHBoxLayout(self)
+
+        # 列表
+        self.listWidget = QListWidget(self)
+        self.listWidget.setFixedWidth(400)
+        layout.addWidget(self.listWidget)
+
+        # //https://y.qq.com/n/ryqq/singer/0044yxPF1Zultc
+        firstPageWidget = PWidget()
+        firstPageWidget.importPath.connect(self.import3rdLib)
+        layout.addWidget(firstPageWidget)
+        mainLay.addLayout(layout)
+
+        hVl = QHBoxLayout(self)
+        # 清空按钮
+        self.clearBtn = QPushButton('清空音色Clear libs', self, objectName="OrangeButton", minimumHeight=48 ,clicked=self.doClearItem)
+        hVl.addWidget(self.clearBtn)
+
+        fBtn=QPushButton("批量导入Import libs", self,
+                                     objectName="OrangeButton", minimumHeight=48 ,clicked=self.open_ni_dir)
+
+        tBtn=QPushButton("捐赠Donate", self,
+                                     objectName="OrangeButton", minimumHeight=48 ,clicked=self.doDonate)
+
+        oBtn=QPushButton("关注作者Follow", self,
+                                     objectName="OrangeButton", minimumHeight=48 ,clicked=self.doFollow)
+
+        eBtn=QPushButton("写邮件Email", self,
+                                     objectName="OrangeButton", minimumHeight=48 ,clicked=self.doEmail)
+
+        hVl.addWidget(fBtn)
+        hVl.addWidget(tBtn)
+        hVl.addWidget(oBtn)
+        hVl.addWidget(eBtn)
+        mainLay.addLayout(hVl)
+        self.setLayout(mainLay)
+
+    def getFullLibs(self):
+        list=list_all_files(TARGET_XML_DIR)
+        for fullname in list:  # 循环读取每一行，1：是从第二行开始
+            if ".xml" in fullname:
+                self.add2listView(fullname)
+
+        # print(self.list)
+    def add2listView(self,fullname):
+        n = judge_ktxml(fullname)
+        if (len(str(n)) < 1):
+            return
+
+        if(fullname in self.list):
+            return
+
+        item = QListWidgetItem(self.listWidget)
+        widget = ItemWidget('已导入Exist:    {}'.format(n), item, self.listWidget)
+        # 绑定删除信号
+        widget.itemDeleted.connect(self.doDeleteItem)
+        self.listWidget.setItemWidget(item, widget)
+        self.list.append(fullname)
+
+    def open_ni_dir(self):
+        if not os.path.exists(TARGET_PLIST_DIR):
+           os.makedirs(TARGET_PLIST_DIR)
+
+        if not os.path.exists(TARGET_XML_DIR):
+           os.makedirs(TARGET_XML_DIR)
+
+        lib_dir = self.dialog.getExistingDirectory(self, "选取文件夹", os.getcwd())
+
+        if len(lib_dir)<1:
+            return
+
+        list = list_all_files(lib_dir)
+        for fullname in list:  # 循环读取每一行，1：是从第二行开始
+            if ".nicnt" in fullname:
+                # print(line)
+                xml = parse_ncint(fullname)
+                # print("parse:" + fullname+"==to=="+xml)
+                self.add2listView(xml)
+
+    def import3rdLib(self,path):
+        xml =parse_ncint(path)
+        self.add2listView(xml)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    app.setStyleSheet(StyleSheet)
+    window = Widget()
+    window.show()
+    # window.test()
+    # window.getFullLibs()
+    # window.open_ni_dir()
+    sys.exit(app.exec())
